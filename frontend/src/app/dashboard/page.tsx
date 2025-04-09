59303c323d4ceb45eafaf982769df769e92c2863
TxDrace <nguyenhaituyen1804@gmail.com>
2025-04-09 21:09:45 +0700

    Fix reload many times error

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -5,16 +5,11 @@ import { useRouter } from "next/navigation";
 import { useAccount } from "wagmi";
 
 export default function DashboardView() {
-  const { isConnected } = useAccount();
   const router = useRouter();
 
   useEffect(() => {
-    if (!isConnected) {
-      router.push("/"); // Redirect to home if wallet is disconnected
-    } else {
-      router.push("/dashboard/overview");
-    }
-  }, [isConnected, router]);
+    router.push("/dashboard/overview");
+  }, []);
 
   return <></>
 }

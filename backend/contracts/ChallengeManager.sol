fce3acb7300925a5f180fde298b0c9d8b776672e
Minh Dat <tmdat21@clc.fitus.edu.vn>
2025-04-23 15:59:28 +0700

    write function contribute and get contributed challenges more details

--- backend/contracts/ChallengeManager.sol
+++ backend/contracts/ChallengeManager.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "hardhat/console.sol";
 
 contract ChallengeManager {
     struct Challenge {
         address contributor;
         string titleUrl;
-        bool isActive;
+        string descriptionUrl;
+        string category;
+        string contributeAt;
+        bool isApproved;
     }
 
     mapping(uint256 => Challenge) public challenges;
@@ -13,28 +17,56 @@ contract ChallengeManager {
 
     mapping(address => uint256[]) public contributorToChallenges;
 
-    event ChallengeContributed(address indexed contributor, string titleUrl);
+    event ChallengeContributed(
+        address indexed contributor,
+        string titleUrl,
+        string descriptionUrl,
+        string category,
+        string contributeAt
+    );
 
-    function contributeChallenge(string calldata titleUrl) external {
+    function contributeChallenge(
+        string calldata titleUrl,
+        string calldata descriptionUrl,
+        string calldata category,
+        string calldata contributeAt
+    ) external {
         uint256 challengeId = totalChallenges++;
 
         challenges[challengeId] = Challenge({
             contributor: msg.sender,
             titleUrl: titleUrl,
-            isActive: false
+            descriptionUrl: descriptionUrl,
+            category: category,
+            contributeAt: contributeAt,
+            isApproved: false
         });
 
         contributorToChallenges[msg.sender].push(challengeId);
 
-        emit ChallengeContributed(msg.sender, titleUrl);
+        console.log(
+            "Challenge #%s contributed by %s with:",
+            challengeId,
+            msg.sender
+        );
+        console.log("- Title url        : %s", titleUrl);
+        console.log("- Description url  : %s", descriptionUrl);
+
+        emit ChallengeContributed(
+            msg.sender,
+            titleUrl,
+            descriptionUrl,
+            category,
+            contributeAt
+        );
     }
 
-    function getChallengesByContributor()
-        public
-        view
-        returns (Challenge[] memory)
-    {
-        uint256[] memory challengeIds = contributorToChallenges[msg.sender];
+    function getChallengesByContributor(
+        address contributor_address
+    ) public view returns (Challenge[] memory) {
+        uint256[] memory challengeIds = contributorToChallenges[
+            contributor_address
+        ];
         Challenge[] memory contributorChallenges = new Challenge[](
             challengeIds.length
         );
@@ -42,6 +74,12 @@ contract ChallengeManager {
         for (uint256 i = 0; i < challengeIds.length; i++) {
             contributorChallenges[i] = challenges[challengeIds[i]];
         }
+
+        console.log(
+            "User %s had fetch %s contributed challenges",
+            contributor_address,
+            challengeIds.length
+        );
 
         return contributorChallenges;
     }

64c4bb053a7b836f5e0829df43b683dfc693c14f
Minh Dat <tmdat21@clc.fitus.edu.vn>
2025-05-04 11:33:36 +0700

    change app font to Nunito Sans and add suppressHydrationWarning

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,19 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 import ThemeProviders from "@/components/layout/theme-providers";
 import { Web3Provider } from "@/features/wallet/Web3Provider";
 import ProtectedRoute from "@/components/layout/protected-route";
 import { ToastContainer } from "react-toastify";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const nunito_sans = Nunito_Sans({
+  weight: ['600', '800'],
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
@@ -33,7 +28,8 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
+        className={`${nunito_sans.className} antialiased`}
       >
         <Web3Provider>
           <ProtectedRoute>

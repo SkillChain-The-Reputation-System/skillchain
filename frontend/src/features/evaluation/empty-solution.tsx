98dcc9347deb4e203499e6d89b4e55c24f90b4aa
Minh Dat <tmdat21@clc.fitus.edu.vn>
2025-05-17 21:21:18 +0700

    design general pages for Evaluation part

---
+++ frontend/src/features/evaluation/empty-solution.tsx
@@ -0,0 +1,46 @@
+'use client'
+// Import hooks
+import { useRouter } from "next/navigation"
+
+// Import UI components
+import { Button } from "@/components/ui/button"
+
+// Import lucide-react icons
+import { FolderX } from "lucide-react"
+
+// Import utils
+import { pageUrlMapping } from "@/constants/navigation"
+
+interface EmptySolutionProps {
+  evaluationWorkspace?: boolean
+}
+
+export function EmptySolution({ evaluationWorkspace }: EmptySolutionProps) {
+  const router = useRouter();
+
+  const handleGoToPendingSolutions = () => {
+    router.push(pageUrlMapping.evaluation_pendingsolutions);
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center py-12 px-4 text-center">
+      <div className="bg-muted/30 rounded-full p-4 mb-4">
+        <FolderX className="h-10 w-10 text-muted-foreground" />
+      </div>
+      <h3 className="text-xl font-bold mb-2">No pending solutions found</h3>
+
+      {evaluationWorkspace ? (
+        <>
+          <p className="text-muted-foreground mb-6 max-w-md">
+            You might have not joined any evaluation for a solution at the moment. Check back later or join now!
+          </p>
+          <Button className="cursor-pointer" onClick={handleGoToPendingSolutions}>Go to Explore</Button>
+        </>
+      ) : (
+        <p className="text-muted-foreground mb-6 max-w-md">
+          There are no challenges available at the moment. Please check back later.
+        </p>
+      )}
+    </div>
+  )
+}
---
+++ frontend/src/features/evaluation/solution-card.tsx
@@ -0,0 +1,101 @@
+// Import UI components
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+
+// Import ludide-react icons
+import {
+  CalendarDays,
+  Check,
+  Eye,
+  Tag,
+  UserRoundPen,
+  LoaderCircle
+} from "lucide-react";
+
+// Import utils
+import { UnderReviewSolutionPreview } from "@/lib/interfaces";
+import {
+  ChallengeSolutionProgress,
+  Domain,
+  DomainLabels,
+} from "@/constants/system";
+import { epochToDateTimeString } from "@/lib/time-utils";
+
+interface SolutionCardProps {
+  solutionPreview: UnderReviewSolutionPreview;
+  onClick: (id: string) => void;
+  forEvaluator?: boolean
+}
+
+export function SolutionCard({ solutionPreview, onClick, forEvaluator = false }: SolutionCardProps) {
+  const submittedDate = epochToDateTimeString(solutionPreview.submittedAt);
+
+  return (
+    <>
+      <Card
+        className="w-full h-full group gap-2 overflow-hidden bg-blue-100 dark:bg-blue-950/60 transition-all duration-300 ease-in-out hover:-translate-y-2 hover:shadow-lg dark:hover:shadow-blue-900/20 border-transparent hover:border-blue-300 dark:hover:border-blue-700 cursor-pointer select-none"
+        onClick={() => onClick(solutionPreview.solutionId)}
+      >
+        <CardHeader>
+          <div className="space-y-1.5">
+            <div className="flex justify-between">
+              <CardTitle className="text-sm font-bold group-hover:text-blue-600 dark:group-hover:text-blue-400 transition-colors">
+                {solutionPreview.challengeTitle}
+              </CardTitle>
+
+              {
+                forEvaluator && (
+                  solutionPreview.progress == ChallengeSolutionProgress.UNDER_REVIEW ?
+                    <LoaderCircle className="h-full max-h-5.5 w-full max-w-5.5 text-yellow-500" />
+                    :
+                    <Check className="h-full max-h-5.5 w-full max-w-5.5 text-green-600" />
+                )
+              }
+            </div>
+
+            <div
+              className="line-clamp-1 mt-1 text-muted-foreground text-xs font-light disable-click-links no-images"
+              dangerouslySetInnerHTML={{ __html: solutionPreview.solution || "" }}
+            />
+          </div>
+        </CardHeader>
+
+        <CardContent className="flex flex-col text-[11px] text-muted-foreground gap-2">
+          <div className="flex items-center gap-1">
+            <UserRoundPen className="h-full max-h-3.5 w-full max-w-3.5" />
+            <span>By</span>
+            <span className="ml-1 text-indigo-800 dark:text-indigo-300 break-all">
+              {solutionPreview.submitter}
+            </span>
+          </div>
+
+          <div className="flex items-center gap-1">
+            <Tag className="h-full max-h-3.5 w-full max-w-3.5" />
+            <p className="ml-1">
+              {DomainLabels[solutionPreview.category as Domain]}
+            </p>
+          </div>
+
+          <div className="flex items-center gap-1">
+            <CalendarDays className="h-full max-h-3.5 w-full max-w-3.5 mr-1" />
+            <span>Submitted {submittedDate}</span>
+          </div>
+
+          <Separator className="my-1 bg-black dark:bg-slate-400" />
+
+          <div className="flex items-center">
+            <Eye className="h-full max-h-3.5 w-full max-w-3.5 mr-1" />
+            <span>
+              {solutionPreview.numberOfEvaluators} / {solutionPreview.totalEvaluators} Evaluators
+            </span>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
+}

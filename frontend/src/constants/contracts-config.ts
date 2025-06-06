60b0de1b364cac5b76faa8a377a8afafe968238a
TxDrace <nguyenhaituyen1804@gmail.com>
2025-06-06 09:05:28 +0700

    SC-25: feat: Remove usage of tiptap pro and edit the script to adjust reputation. A bug happen due to the change in contract address.

--- frontend/src/constants/contracts-config.ts
+++ frontend/src/constants/contracts-config.ts
@@ -37,31 +37,31 @@ export const ContractConfig_EvaluationEscrow = {
 };
 
 export const ContractConfig_JobApplicationManager = {
-    address: '0xc6e7DF5E7b4f2A278906862b61205850344D4e7d', // Change this every time you deploy the contract
+    address: '0x59b670e9fA9D0A427751Af201D676719a970857b', // Change this every time you deploy the contract
     abi: JobApplicationManagerArtifact.abi,
 };
 
 export const ContractConfig_JobManager = {
-    address: '0x59b670e9fA9D0A427751Af201D676719a970857b', // Change this every time you deploy the contract
+    address: '0x4ed7c70F96B99c776995fB64377f0d4aB3B0e1C1', // Change this every time you deploy the contract
     abi: JobManagerArtifact.abi,
 };
 
 export const ContractConfig_MeetingManager = {
-    address: '0x322813Fd9A801c5507c9de605d63CEA4f2CE6c44', // Change this every time you deploy the contract
+    address: '0xa85233C63b9Ee964Add6F2cffe00Fd84eb32338f', // Change this every time you deploy the contract
     abi: MeetingManagerArtifact.abi,
 };
 
 export const ContractConfig_ModerationEscrow = {
-    address: '0xa85233C63b9Ee964Add6F2cffe00Fd84eb32338f', // Change this every time you deploy the contract
+    address: '0x4A679253410272dd5232B3Ff7cF5dbB88f295319', // Change this every time you deploy the contract
     abi: ModerationEscrowArtifact.abi,
 };
 
 export const ContractConfig_RecruiterDataManager = {
-    address: '0x09635F643e140090A9A8Dcd712eD6285858ceBef', // Change this every time you deploy the contract
+    address: '0x67d269191c92Caf3cD7723F116c85e6E9bf55933', // Change this every time you deploy the contract
     abi: RecruiterDataManagerArtifact.abi,
 };
 
 export const ContractConfig_UserDataManager = {
-    address: '0xc5a5C42992dECbae36851359345FE25997F5C42d', // Change this every time you deploy the contract
+    address: '0xE6E340D132b5f46d1e472DebcD681B2aBc16e57E', // Change this every time you deploy the contract
     abi: UserDataManagerArtifact.abi,
 };

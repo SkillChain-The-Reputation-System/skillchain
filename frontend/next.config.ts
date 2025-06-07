8fdc7dbd8bbfc7249b40f62488b03b413c1256d8
TxDrace <nguyenhaituyen1804@gmail.com>
2025-06-07 17:17:37 +0700

    feat: Fix running error when run project without turboback

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,39 +5,48 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const nextConfig: NextConfig = {
   devIndicators: false,
   images: {
-    domains: ['gateway.irys.xyz'],
+    domains: ["gateway.irys.xyz"],
   },
-  webpack: (config) => {
-    config.plugins = (config.plugins || []).concat([
-      new NodePolyfillPlugin(),
-      new webpack.ProvidePlugin({
-        process: "process/browser.js",
-      }),
-    ]);
+  webpack: (config, { isServer }) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "pino-pretty": false,
+    };
+    if (!isServer) {
+      config.plugins = (config.plugins || []).concat([
+        new NodePolyfillPlugin(),
+        new webpack.ProvidePlugin({
+          process: "process/browser.js",
+        }),
+      ]);
 
-    const fallback = config.resolve.fallback || {};
-    Object.assign(fallback, {
-      crypto: require.resolve("crypto-browserify"),
-      stream: require.resolve("stream-browserify"),
-      assert: require.resolve("assert"),
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      os: require.resolve("os-browserify"),
-      url: require.resolve("url"),
-      zlib: require.resolve("browserify-zlib"),
-      buffer: require.resolve("buffer"),
-      path: require.resolve("path-browserify"),
-    });
+      const fallback = config.resolve.fallback || {};
+      Object.assign(fallback, {
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
+        assert: require.resolve("assert"),
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        os: require.resolve("os-browserify"),
+        url: require.resolve("url"),
+        zlib: require.resolve("browserify-zlib"),
+        buffer: require.resolve("buffer"),
+        path: require.resolve("path-browserify"),
+      });
 
-    config.resolve.fallback = fallback;
-    config.resolve.extensions = [
-      ".js",
-      ".jsx",
-      ".json",
-      ".mjs",
-      ".wasm",
-      ".css",
-    ];
+      config.resolve.fallback = fallback;
+      config.resolve.extensions = [
+        ".ts",
+        ".tsx",
+        ".js",
+        ".jsx",
+        ".json",
+        ".mjs",
+        ".wasm",
+        ".css",
+      ];
+    }
 
     return config;
   },

95bded6be1da1db97dcd4fc0d6ce4a1e0e0c9960
Trần Minh Đạt <minhdattran1199@gmail.com>
2025-06-14 19:28:13 +0700

    update tab Evaluation

---
+++ frontend/src/app/dashboard/evaluation/evaluated-by-me/[solutionId]/page.tsx
@@ -0,0 +1,26 @@
+import EvaluationDetail from '@/features/evaluation/evaluated-by-me/evualation-detail';
+import { pageUrlMapping } from "@/constants/navigation";
+import { MoveLeft } from "lucide-react";
+import Link from "next/link";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ solutionId: `0x${string}` }>;
+}) {
+  const { solutionId } = await params;
+
+  return (
+    <div className="flex flex-col px-12">
+      <Link
+        href={pageUrlMapping.evaluation_evaluatedbyme}
+        className="flex gap-2 items-center mb-10 text-zinc-700 hover:underline hover:underline-offset-4 dark:text-zinc-300"
+      >
+        <MoveLeft className="h-4 w-4" />
+        Back to Your Evaluations
+      </Link>
+
+      <EvaluationDetail solutionId={solutionId} />
+    </div>
+  );
+}
---
+++ frontend/src/app/dashboard/evaluation/pending-solutions/[solutionId]/page.tsx
@@ -0,0 +1,26 @@
+import SolutionDetail from "@/features/evaluation/pending-solutions/solution-detail";
+import { MoveLeft } from "lucide-react";
+import Link from "next/link";
+import { pageUrlMapping } from "@/constants/navigation";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ solutionId: `0x${string}` }>;
+}) {
+  const { solutionId } = await params;
+
+  return (
+    <div className="flex flex-col px-12">
+      <Link
+        href={pageUrlMapping.evaluation_pendingsolutions}
+        className="flex gap-2 items-center mb-10 text-zinc-700 hover:underline hover:underline-offset-4 dark:text-zinc-300"
+      >
+        <MoveLeft className="h-4 w-4" />
+        Back to Solutions Explore
+      </Link>
+
+      <SolutionDetail solutionId={solutionId} />
+    </div>
+  );
+}
